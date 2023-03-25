5d82b10018c6f91a4a63d60a82272142ae24b9d1
eli-linux-vm <etl3@calvin.edu>
2023-03-25 08:25:47 -0400

    buffers or somethuing

--- asteroid.h
+++ asteroid.h
@@ -4,10 +4,14 @@ class Asteroid {
     public:
         glm::vec3 location;
         glm::vec3 velocity;
+        int mass, size, density;
 
-        Asteroid(float x, float y, float z, float velx, float vely, float velz) {
+        Asteroid(double x, double y, double z, float velx, float vely, float velz, float size, float density) {
             location = glm::vec3(x, y, z);
             velocity = glm::vec3(velx, vely, velz);
+            size = size;
+            density = density;
+            mass = size * density;
         }
         
 };
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <cassert>
 #include <cstring>
+#include <time.h>
+#include <stdlib.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <cmath>
@@ -11,18 +13,22 @@
 #include "cubeverts.h"
 #include "asteroid.h"
 
+//debugs
+#include "glm/gtx/string_cast.hpp"
+
 /////////////////////////////////////////////////////
 // I truly am sorry if you have to read this code  //
 // I'm serious.                         Eli Lewis  //
+// 🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝🍝  //
 /////////////////////////////////////////////////////
 
 // significant help from docs at https://learnopengl.com/
 
 // global vars
 unsigned int shaderProgram;
-unsigned int VBO, VAO, EBO;
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+unsigned int VBO, VAO, LVBO;
+unsigned int SCR_WIDTH = 800;
+unsigned int SCR_HEIGHT = 600;
 
 const std::string vertex_shader_source_file = "./vertexshader.glsl";
 const std::string fragment_shader_source_file = "./fragmentshader.glsl";
@@ -42,6 +48,7 @@ bool firstMouse = true;
 Asteroid * asteroids;
 
 // function defs
+std::pair<int, int> get_resolution();
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 int compile_shaders();
@@ -50,11 +57,16 @@ void create_vbos();
 void bind_buffer_data(int buffer, float * vertices, int vertices_size);
 void loadShader(std::string filename, std::string &shader);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+float get_rand_in_range( float min, float max );
+
 
 int main()
 {
     // glfw: initialize and configure
     // ------------------------------
+    
+    srand(time(NULL));   //init random numbers
+    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -64,9 +76,13 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
+    std::pair<int, int> p = get_resolution();
+    SCR_WIDTH = p.first;
+    SCR_HEIGHT = p.second;
+
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "main", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH-(int)SCR_WIDTH*0.2f, SCR_HEIGHT-(int)SCR_HEIGHT*0.2f, "main", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -76,7 +92,7 @@ int main()
 
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+    
     // glad: load all OpenGL function pointers
     // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -94,12 +110,17 @@ int main()
     }
 
     glEnable(GL_DEPTH_TEST); 
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
+    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
+    // I think i found a bug in glfw 
     glfwSetCursorPosCallback(window, mouse_callback);  
+
+    float lines[] = {0.0, 0.0, 0.0,
+                    1.0, 1.0, 1.0}; 
 
     create_vaos();
     create_vbos();
     bind_buffer_data(VBO, vertices, vertices_size);
+    //bind_buffer_data(LVBO, lines, 2*3);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -115,18 +136,26 @@ int main()
     glm::mat4 model         = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
     glm::mat4 projection    = glm::mat4(1.0f);
     projection = glm::perspective(glm::radians(65.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    
     int mpvLoc = glGetUniformLocation(shaderProgram, "mpv");
+    int colorLoc = glGetUniformLocation(shaderProgram, "color");
 
-    /*int number_of_asteroids = 10;
+    int number_of_asteroids = 300;
     asteroids = (Asteroid*)malloc(sizeof(Asteroid) * number_of_asteroids);
     for (int i = 0; i < number_of_asteroids; i++){
-        asteroids[i] = new Asteroid(glm::vec3(1.0f * i + 5.0f, 1.0f * i + 5.0f, 1.0f * i + 5.0f), glm::vec3(0.0f));
-    }*/
+        asteroids[i] = Asteroid(
+                get_rand_in_range(-45.0f, 45.0f), //x
+                get_rand_in_range(-45.0f, 45.0f), //y
+                get_rand_in_range(-45.0f, 45.0f), //z
+                get_rand_in_range(-10.0f, 10.0f), //velx
+                get_rand_in_range(-10.0f, 10.0f), //vely
+                get_rand_in_range(-10.0f, 10.0f), //velz
+                get_rand_in_range(1.0f, 5.0f), //size
+                get_rand_in_range(0.01f, 1.0f)); //density
+    }
     
-    Asteroid a(1.0f, 1.0f, 5.0f, 0.0f, 0.0f, 0.0f);
-    std::cout << a.location[0] << std::endl;
-    std::cout << a.location[1] << std::endl;
-    std::cout << a.location[2] << std::endl;
+    //Asteroid a(1.0f, 1.0f, 5.0f, 0.0f, 0.0f, -3.0f);
+    
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -145,20 +174,36 @@ int main()
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        //glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-        //for(unsigned int i = 0; i < number_of_asteroids; i++)
+        for(unsigned int i = 0; i < number_of_asteroids; i++)
         {
-           
             glm::mat4 model = glm::mat4(1.0f);
-            model = glm::translate(model, a.location);
-            //model = glm::rotate(model, glm::radians(10.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-            //model = glm::rotate(model, glm::radians(10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-            //model = glm::rotate(model, glm::radians(10.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-            glm::mat4 mpv = projection * view * model;
-            glDrawArrays(GL_TRIANGLES, 0, 36);
+            model = glm::translate(model, asteroids[i].location);
+            model = glm::rotate(model, asteroids[i].velocity.x * asteroids[i].location.x * glm::radians(10.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+            model = glm::rotate(model, asteroids[i].velocity.y * asteroids[i].location.y * glm::radians(10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+            model = glm::rotate(model, asteroids[i].velocity.z * asteroids[i].location.z * glm::radians(10.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+            //model = glm::scale(model, glm::vec3(asteroids[i].size, asteroids[i].size, asteroids[i].size));            
+
+            asteroids[i].location += asteroids[i].velocity * deltaTime;
+            //asteroids[i].velocity *= 0.99f; //friction
+            
+            if (glm::distance(asteroids[i].location, cameraPos) > 85){
+                asteroids[i].velocity *= -1; 
+            }
+
+            glUniform3fv(colorLoc, 1, glm::value_ptr(glm::vec3(asteroids[i].mass, asteroids[i].density, asteroids[i].size)));
+
+            glm::mat4 mpv = projection * view * model;            
             glUniformMatrix4fv(mpvLoc, 1, GL_FALSE, glm::value_ptr(mpv));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+         
         }
 
+        //glBindBuffer(GL_ARRAY_BUFFER, LVBO);
+        //glDrawArrays(GL_LINES, 0, 2);
+        
+        //glBindBuffer(GL_ARRAY_BUFFER, 0);
         // handle camera
         view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
@@ -172,6 +217,7 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    //glDeleteBuffers(1, &LVBO);
     glDeleteProgram(shaderProgram);
 
     free(asteroids);
@@ -186,7 +232,10 @@ int main()
 // Handle mouse events
 // ---------------------------------------------------------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos){  
-
+  
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE) 
+        return;
+    
     if (firstMouse)
     {
         lastX = xpos;
@@ -206,11 +255,14 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos){
     yaw   += xoffset;
     pitch += yoffset;
 
+    /*
     if(pitch > 89.0f)
         pitch =  89.0f;
     if(pitch < -89.0f)
         pitch = -89.0f;
+    */
 
+    //magic
     direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
     direction.y = sin(glm::radians(pitch));
     direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
@@ -305,11 +357,14 @@ int compile_shaders(){
 
 void create_vbos(){
     glGenBuffers(1, &VBO);
+    //glGenBuffers(1, &LVBO); 
 }
+
 
 void bind_buffer_data(int buffer, float * vertices, int vertices_size){
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(float)*vertices_size, vertices, GL_STATIC_DRAW);
+    //glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void create_vaos(){
@@ -317,9 +372,6 @@ void create_vaos(){
     glBindVertexArray(VAO);
 }
 
-void create_ebos(){
-    glGenBuffers(1, &EBO);
-}
 
 void loadShader(std::string filename, std::string &shader){
     std::ifstream fin(filename.c_str());
@@ -331,5 +383,18 @@ void loadShader(std::string filename, std::string &shader){
     }
 
     fin.close();
+}
+
+std::pair<int, int> get_resolution()
+{
+    const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    return std::make_pair(mode->width, mode->height);
+}
+
+// -------------------------------------
+float get_rand_in_range( float min, float max )
+{
+    float scale = rand() / (float) RAND_MAX; 
+    return min + scale * ( max - min );      
 }
 
