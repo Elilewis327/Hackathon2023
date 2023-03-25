fc6542f467984278c2ad5b2d05eb2e9b6d87def9
eli-linux-vm <etl3@calvin.edu>
2023-03-25 04:14:36 -0400

    d

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jan/Bin/Hackathon
-# Build directory: /Users/jan/Bin/Hackathon
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("glad")
--- glad/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jan/Bin/Hackathon/glad
-# Build directory: /Users/jan/Bin/Hackathon/glad
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
